54a5a3fd2fc573b4b0d53659a5e7a3932cbc545a
yacoubi mohamed amine <aminecnam@gmail.com>
2024-05-09 17:48:21 +0100

    yaakoubi med amine assignement nested dictionaries & lists

--- fundamentals/fundamentals/nested_dictionaries_&_lists/yaakoubi_med_amine_assignement_nested_dictionaries_and_lists.py
+++ fundamentals/fundamentals/nested_dictionaries_&_lists/yaakoubi_med_amine_assignement_nested_dictionaries_and_lists.py
@@ -35,3 +35,29 @@ students = [
 res = iterateDictionary(students) 
 for i in res:
     print(i[0] + ", " +i[1])
+
+
+###### 3 -Get Values From a List of Dictionaries #######
+
+def iterateDictionary2(key_name,some_list):
+    for i in range(len(some_list)):
+        print (some_list[i][key_name])
+
+key_name_f = "first_name"
+key_name_l = "last_name"
+iterateDictionary2(key_name_f,students)
+iterateDictionary2(key_name_l,students)
+
+##### 4 -Iterate Through a Dictionary with List Values ######
+
+def printInfo(dic):
+    for key,val in dic.items():
+        print (str(len(val)), key)
+        for j in val:
+            print (j)
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
