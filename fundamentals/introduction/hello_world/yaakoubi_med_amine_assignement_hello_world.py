2e8cc0ef928c77ce60e43d325d9f22dda5cfa7af
yacoubi mohamed amine <aminecnam@gmail.com>
2024-05-09 11:13:23 +0100

    assignement hello world

--- fundamentals/introduction/hello_world/yaakoubi_med_amine_assignement_hello_world.py
+++ fundamentals/introduction/hello_world/yaakoubi_med_amine_assignement_hello_world.py
@@ -1,13 +1,13 @@
 # 1. TASK: print "Hello World"
 print("Hello World")
 # 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
+name = "mohamed amine"
 print('Hello',name,'!') # with a comma
 print( "Hello "+name+"!" )	# with a +
 # 3. print "Hello 42!" with the number in a variable
-name = 42
-print( "Hello",str(name),"!" )	# with a comma
-print( "Hello "+str(name)+"!" )	# with a +	-- this one should give us an error!
+num = 38
+print( "Hello",str(num),"!" )	# with a comma
+print( "Hello "+str(num)+"!" )	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
