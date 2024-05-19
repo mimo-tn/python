69b9b0175ef9e75376a64a3eb07c0bbc6a353ffe
yacoubi mohamed amine <aminecnam@gmail.com>
2024-05-19 12:43:54 +0100

    yaakoubi med amine assignment selection sort

--- fundamentals/extras/selection sort/selection_sort.py
+++ fundamentals/extras/selection sort/selection_sort.py
@@ -1,7 +1,4 @@
 new_list = [8,6,4,9,5,2,1,7]
-min = new_list[0]
-position_min = 0
-index = 0
 for i in range(len(new_list) - 1):
     min = new_list[i]
     print("****"*20,"\n",f"itiration numer {i}")
@@ -9,6 +6,5 @@ for i in range(len(new_list) - 1):
         if new_list[j] < min :
             min = new_list[j]
             new_list[i],new_list[j] = new_list[j],new_list[i]
-            position_min = j
     print(f"in this itiration this is the result of our sort algorithm {new_list}")
 print(f"this is the list sorted {new_list}")
