3b2885c422f479424fc0ef20dde0ba339fe4ef5c
yacoubi mohamed amine <aminecnam@gmail.com>
2024-05-31 17:31:46 +0100

    assignment yaakoubi mohamed amine understanding routing

---
+++ flask/fundamentals/Understanding_Routing/server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "Hello world!"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<string:name>')
+def name(name):
+    return f"Hi {name}!"
+
+@app.route('/repeat/<string:variable>/<int:numbers>')
+def repeat(variable,numbers):
+    return f"{variable * numbers}"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Sorry! No response. Try again."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
