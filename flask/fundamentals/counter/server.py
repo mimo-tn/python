66ef058235d680b4f1cec393a5c6fc1a7d631440
yacoubi mohamed amine <aminecnam@gmail.com>
2024-06-02 13:30:49 +0100

    yaakoubi med amine assignment counter

---
+++ flask/fundamentals/counter/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    count = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    print(f"charging {request.form['first_name']} for {count} ")
+    return render_template("checkout.html")
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
